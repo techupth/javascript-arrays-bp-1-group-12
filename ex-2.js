1ebad814e1f96a6bcb353800ad7a5c7bf1d9f189
James <Natasan2121@gmail.com>
2023-09-25 00:20:09 +0700

    complete arrays assignment

--- ex-2.js
+++ ex-2.js
@@ -12,7 +12,28 @@ const employees = [
   },
 ];
 // Start coding here
-let alex;
-let alexHobbies;
-let alexAndJamesAge;
+let alex = employees[1].name;
+employees[1].hobbies.push("Watching basketball")
+let alexHobbies = employees[1].hobbies;
+let alexAndJamesAge = employees[0].age + employees[1].age ;
 const updateEmployees = employees;
+
+updateEmployees[2] = {
+  name: "Alicia",
+  age: 29,
+  hobbies: ["Shopping", "Reading novels"]
+
+}
+
+updateEmployees[3] = {
+  name: "Kody",
+  age: 19,
+  hobbies: ["Computer games", "Wakeboard"]
+
+}
+console.log(updateEmployees)
+
+updateEmployees.pop()
+
+console.log(employees)
+console.log(updateEmployees)
